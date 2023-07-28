1936ed0715904efc34e256dd2773ab6e0414c66b
Vania Yarovyi <yarovyi@gmail.com>
2023-07-28 22:31:57 +0300

    added lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,1 @@
-console.log('Hello')
-
-const getNewId = (items) => {
-  
-  
-  if (items.length == 0) {
-    return 1;
-  
-  }
-
-  const ids = ?items.map((i) => i.id);
-
-  return Math.max(...ids) + 1;
-};
+console.log('Hello');
