48286014b7f6ea1ca24f16ebb1dc93ea82c4b8be
Maksym Nemera <nickmitchell@ukr.net>
2023-07-31 18:34:42 +0300

    sync data products with db

---
+++ src/controllers/Products.controllers.ts
@@ -0,0 +1,66 @@
+'use strict';
+
+import { Request, Response } from 'express';
+import { Products } from '../models/products.model';
+import { ProductData } from '../types/Products';
+
+const normalize = ({
+  id,
+  category,
+  itemId,
+  name,
+  fullPrice,
+  price,
+  screen,
+  capacity,
+  color,
+  ram,
+  year,
+  image,
+}: ProductData): ProductData => {
+  return {
+    id,
+    category,
+    itemId,
+    name,
+    fullPrice,
+    price,
+    screen,
+    capacity,
+    color,
+    ram,
+    year,
+    image,
+  };
+};
+
+const getAllProducts = async (req: Request, res: Response) => {
+  const products = await Products.findAll();
+
+  res.json(products.map(normalize));
+};
+
+const getOneProduct = async (req: Request, res: Response) => {
+  const { productId } = req.params;
+
+  if (!Number(productId)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const foundProduct = await Products.findByPk(productId);
+
+  if (!foundProduct) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.json(normalize(foundProduct));
+};
+
+export const productsController = {
+  getAllProducts,
+  getOneProduct,
+};
---
+++ src/types/Products.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+export interface ProductData {
+  id?: number;
+  category: string;
+  itemId: string;
+  name: string;
+  fullPrice: number;
+  price: number;
+  screen: string;
+  capacity: string;
+  color: string;
+  ram: string;
+  year: number;
+  image: string;
+}
