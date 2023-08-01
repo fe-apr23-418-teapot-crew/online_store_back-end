5271c60ff8cc4e5a06498be80853c1545c480f17
Maksym Bobryk <booobryyyk@gmail.com>
2023-08-01 19:02:15 +0300

    fixed down method in seeder

--- seeders/20230801152936-add-products.js
+++ seeders/20230801152936-add-products.js
@@ -2,20 +2,18 @@
 
 import products from '../src/utils/db/api/products.json';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function up(queryInterface, Sequelize) {
-  await queryInterface.bulkInsert(
-    'products',
-    products,
-  );
-}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function down(queryInterface, Sequelize) {
-  await queryInterface.bulkDelete(
-    'products',
-    {
-      name: products.map(({ name }) => name)
-    },
-  );
-}
+module.exports = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'products',
+      products,
+    );
+  },
+  
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('products', null, {});
+  }
+};
