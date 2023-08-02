13b766cc07d6d227ccb9946efcbb20c84c623142
Maksym Bobryk <booobryyyk@gmail.com>
2023-08-02 18:51:24 +0300

    fixed migrations and seeders

--- seeders/20230801152936-add-products.js
+++ seeders/20230801152936-add-products.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import products from '../src/utils/db/api/products.json';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const products = require('../src/utils/db/api/products.json');
 
 module.exports = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
